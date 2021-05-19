b383d0de10227b03a3dc9530d50fff996e3691d4
ludovitsiket <siket.ludovit@gmail.com>
2021-05-19 13:49:20 +0200

    Other changes.
    Functions control_extension(), cont_repl().

--- vcfm.py
+++ vcfm.py
@@ -9,7 +9,14 @@ def read_file(contact_file):
 
 def write_file(contact_file, content):
     with open(contact_file, 'w') as c:
-        return c.write(content)
+        return c.write(str(content))
+
+
+def cont_repl(value, content, value2):
+    for item in range(len(value)):
+        content = content.replace(value[item], "")
+        content = content.replace(value2, "---")
+    return content
 
 
 def extract(content):
@@ -21,22 +28,28 @@ def extract(content):
         ";;;",
         "END:VCARD"]
     to_replace = "BEGIN:VCARD"
-
-    for item in range(len(unnecessary_stuffs)):
-        content = content.replace(unnecessary_stuffs[item], "")
-        content = content.replace(to_replace, "---")
+    content = cont_repl(unnecessary_stuffs, content, to_replace)
     return content
+
+
+def control_extension(value):
+    if value.lower().endswith('.vcf'):
+        return True
+    else:
+        return False
 
 
 def main():
     try:
-        origin_content = read_file(sys.argv[1])
-        content = extract(origin_content)
-        write_file("phone.txt", content)
-        print('OK')
-    except IndexError as e:
+        if control_extension(sys.argv[1]) is True:
+            origin_content = read_file(sys.argv[1])
+            write_file("phone.txt", extract(origin_content))
+            print('OK')
+        else:
+            print('Argument is not valid file')
+            sys.exit()
+    except (IndexError, FileNotFoundError) as e:
         print(e)
-        sys.exit()
 
 
 main()
